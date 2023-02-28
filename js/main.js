bd65a13f982ee5214e2210600725f4c1c6dd4f6e
n0g4y0 <n0g4y0@gmail.com>
2023-02-28 08:32:20 -0400

    agrega funciones CORE/principales del juego

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 
+let listaSecuenciaJuego = []
+let listaSecuenciaUsuario = []
+
+class Pieza{
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+    get value(){
+        return  this.nombre[0].toUpperCase();
+    }
+}
+
+let valores = [new Pieza('blue'),new Pieza('yellow'),new Pieza('red'),new Pieza('green')];
+
+function obtenerValorAleatorio(valores){
+
+    let valorAleatorio =  Math.floor(Math.random() * (((valores.length) - 0) + 0));
+    return valores[valorAleatorio];
+}
+
+function agregarValorALista(lista,listaOpciones){
+
+    lista.push(obtenerValorAleatorio(listaOpciones));
+}
+
+function secuenciasSonIguales(listaSecuenciaJuego, listaSEcuenciaUsuario){
+
+    if(listaSecuenciaJuego.length !== listaSEcuenciaUsuario.length){
+        return false;
+    }else{
+        return !listaSecuenciaJuego.some((item,i) => item.value !== listaSEcuenciaUsuario[i].value)
+    }
+}
+
+
