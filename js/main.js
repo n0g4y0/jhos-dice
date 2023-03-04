b597613e50428124ad977944ff87e6707684c609
n0g4y0 <n0g4y0@gmail.com>
2023-03-04 08:38:49 -0400

    agrega eventos de los botones

--- js/main.js
+++ js/main.js
@@ -33,4 +33,44 @@ function secuenciasSonIguales(listaSecuenciaJuego, listaSEcuenciaUsuario){
     }
 }
 
+function resaltarPieza(name){
+    
+    let $elemento = document.querySelector(`#${name}`);
+    $elemento.classList.add(`resaltar-${name}`);
+    console.log(`agregando super ${name}`);
+
+    setTimeout( function(){
+        mostrarPorTiempo(name);
+    },1000,name);
+
+}
+
+
+function mostrarPorTiempo(name){
+
+    let $elemento = document.querySelector(`#${name}`);
+    $elemento.classList.remove(`resaltar-${name}`);
+    console.log(`eliminando super ${name}`);
+
+}
+
+
+// EVENTOS
+
+document.querySelector("#blue").onclick = function(event){
+    resaltarPieza(event.target.value);
+}
+
+document.querySelector("#red").onclick = function(event){
+    resaltarPieza(event.target.value);
+}
+
+document.querySelector("#yellow").onclick = function(event){
+    resaltarPieza(event.target.value);
+}
+
+document.querySelector("#green").onclick = function(event){
+    resaltarPieza(event.target.value);
+}
+
 
